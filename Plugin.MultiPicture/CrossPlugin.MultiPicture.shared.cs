0a4fcf9502951822cba664d1737188c8475ab9ae
Saimel <saimel.usa@gmail.com>
2019-07-16 10:29:58 -0400

    Add project files.

---
+++ Plugin.MultiPicture/CrossPlugin.MultiPicture.shared.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Plugin.Plugin.MultiPicture
+{
+    /// <summary>
+    /// Cross Plugin.MultiPicture
+    /// </summary>
+    public static class CrossPlugin.MultiPicture
+    {
+        static Lazy<IPlugin.MultiPicture> implementation = new Lazy<IPlugin.MultiPicture>(() => CreatePlugin.MultiPicture(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+    /// <summary>
+    /// Gets if the plugin is supported on the current platform.
+    /// </summary>
+    public static bool IsSupported => implementation.Value == null ? false : true;
+
+    /// <summary>
+    /// Current plugin implementation to use
+    /// </summary>
+    public static IPlugin.MultiPicture Current
+    {
+        get
+        {
+            IPlugin.MultiPicture ret = implementation.Value;
+            if (ret == null)
+            {
+                throw NotImplementedInReferenceAssembly();
+            }
+            return ret;
+        }
+    }
+
+    static IPlugin.MultiPicture CreatePlugin.MultiPicture()
+    {
+#if NETSTANDARD1_0 || NETSTANDARD2_0
+            return null;
+#else
+#pragma warning disable IDE0022 // Use expression body for methods
+        return new Plugin.MultiPictureImplementation();
+#pragma warning restore IDE0022 // Use expression body for methods
+#endif
+    }
+
+    internal static Exception NotImplementedInReferenceAssembly() =>
+        new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+
+}
+}
---
+++ Plugin.MultiPicture/Plugin.MultiPicture.uwp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.Plugin.MultiPicture
+{
+    /// <summary>
+    /// Interface for Plugin.MultiPicture
+    /// </summary>
+    public class Plugin.MultiPictureImplementation : IPlugin.MultiPicture
+    {
+    }
+}
